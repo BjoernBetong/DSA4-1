cb3d8c1b078b564094951fc77ef771b138690775
BjoernBetong <145716744+BjoernBetong@users.noreply.github.com>
2024-03-07 10:55:14 +0100

    Update and rename Elementarer_Diener_v10.js to Elementarer_Diener.js
    Sonderfertigkeiten, Vor- und Nachteile werden automatisch eingerechnet

--- Elementarer_Diener.js
+++ Elementarer_Diener.js
@@ -47,7 +47,43 @@ async function main() {
 		const ElementarerDiener = token.actor.items.find(item => item.name === "Elementarer Diener");																								//Suche nach dem Zauber "Elementarer Diener"
 		const ElementarerDienerValue = (ElementarerDiener === undefined)? isNaN : (ElementarerDiener.system.value === null)? 0 : ElementarerDiener.system.value;									//Abrufen des ZfW des Zaubers "Elementarer Diener"
 
+	//Vor- und Nachteile
+		const Affinity = token.actor.items.find(item => item.name === "Affinität zu  Elementaren");
+		const AffinityMod = (Affinity === undefined)? 0 : 3;
+		const BGFire = token.actor.items.find(item => item.name === "Begabung für  Elementar (Feuer)");
+		const BGFireMod = (BGFire === undefined)? 0 : 2;
+		const BGWater = token.actor.items.find(item => item.name === "Begabung für  Elementar (Wasser)");
+		const BGWaterMod = (BGWater === undefined)? 0 : 2;
+		const BGHumus = token.actor.items.find(item => item.name === "Begabung für  Elementar (Humus)");
+		const BGHumusMod = (BGHumus === undefined)? 0 : 2;
+		const BGIce = token.actor.items.find(item => item.name === "Begabung für  Elementar (Eis)");
+		const BGIceMod = (BGIce === undefined)? 0 : 2;
+		const BGOre = token.actor.items.find(item => item.name === "Begabung für  Elementar (Erz)");
+		const BGOreMod = (BGOre === undefined)? 0 : 2;
+		const BGAir = token.actor.items.find(item => item.name === "Begabung für  Elementar (Luft)");
+		const BGAirMod = (BGAir === undefined)? 0 : 2;
+		const CheckMage = token.actor.items.find(item => item.name === "Repräsentation: Magier");
+		const CheckMageMod = (CheckMage === undefined)? false : true;
+		const CheckDruid = token.actor.items.find(item => item.name === "Repräsentation: Druide");
+		const CheckDruidMod = (CheckDruid === undefined)? false : true;
+		const CheckGeode = token.actor.items.find(item => item.name === "Repräsentation: Geode");
+		const CheckGeodeMod = (CheckGeode === undefined)? false : true;
+		const CheckAchaz = token.actor.items.find(item => item.name === "Repräsentation: Achaz");
+		const CheckAchazMod = (CheckAchaz === undefined)? false : true;
+
 	//Sonderfertigkeiten des Helden
+		const MKFire = token.actor.items.find(item => item.name === "Merkmalskenntnis: Elementar (Feuer)");
+		const MKFireMod = (MKFire === undefined)? 0 : (MKFire.system.value === null)? 0 : 2;
+		const MKWater = token.actor.items.find(item => item.name === "Merkmalskenntnis: Elementar (Wasser)");
+		const MKWaterMod = (MKWater === undefined)? 0 : (MKWater.system.value === null)? 0 : 2;
+		const MKHumus = token.actor.items.find(item => item.name === "Merkmalskenntnis: Elementar (Humus)");
+		const MKHumusMod = (MKHumus === undefined)? 0 : (MKHumus.system.value === null)? 0 : 2;
+		const MKIce = token.actor.items.find(item => item.name === "Merkmalskenntnis: Elementar (Eis)");
+		const MKIceMod = (MKIce === undefined)? 0 : (MKIce.system.value === null)? 0 : 2;
+		const MKOre = token.actor.items.find(item => item.name === "Merkmalskenntnis: Elementar (Erz)");
+		const MKOreMod = (MKOre === undefined)? 0 : (MKOre.system.value === null)? 0 : 2;
+		const MKAir = token.actor.items.find(item => item.name === "Merkmalskenntnis: Elementar (Luft)");
+		const MKAirMod = (MKAir === undefined)? 0 : (MKAir.system.value === null)? 0 : 2;
 		const ZSFire = token.actor.items.find(item => item.name === "Zauberspezialisierung Elementarer Diener [Magier] (Elementar (Feuer)) (Elementarer Diener)");									//Suche nach einer Zauberspezialisierung für "Elementarer Diener (Feuer)"
 		const ZSFireValue = (ZSFire === undefined)? 0 : (ZSFire.system.value === null)? 0 : 2;																										//Zuweisen des ZfW Bonus für die Zauberspezialisierung
 		const ZSWater = token.actor.items.find(item => item.name === "Zauberspezialisierung Elementarer Diener [Magier] (Elementar (Wasser)) (Elementarer Diener)");								//Suche nach einer Zauberspezialisierung für "Elementarer Diener (Wasser)"
@@ -98,21 +134,40 @@ async function main() {
         `+ divFlexEnd	
     inputDialog += divFlexStart + "Beschwörungsschwierigkeit: <input id='summonDiff'" + divInputNumber  + "0'/>" + divFlexEnd;
 	inputDialog += divFlexStart + "Kontrollschwierigkeit: <input id='controlDiff'" + divInputNumber  + "0'/>" + divFlexEnd;
-    inputDialog += divFlexStart + "Wahrer Name: <input id='trueName'" + divInputNumber  + "0'/>" + divFlexEnd;
+    inputDialog += divFlexStart + "Wahrer Name: <input id='trueName'" + divInputNumber  + "0'/>" + divFlexEnd + hr;
+	inputDialog += "<h3><b>Modifikationen der Beschwörungsprobe</b><br></h3>"
+	inputDialog += divFlexStart + `
+            <label for="checkGarment">richtige Gewandung</label><input type="checkbox" id="checkGarment" name="checkGarment" style="float:right">
+        `+ divFlexEnd;
+	inputDialog += divFlexStart + `
+            <form action"#">
+                 <label for="purity">Reinheit/Menge des Elements</label>
+                 <select name="purity" id="purity" style="float:right">
+                    <option value="0">-3</option>
+                    <option value="1">-2</option>
+                    <option value="2">-1</option>
+					<option value="3" selected>0</option>
+                    <option value="4">+1</option>
+                    <option value="5">+2</option>
+                    <option value="6">+3</option>
+                 </select>
+            </form>
+        `+ divFlexEnd + hr;
+	inputDialog += "<h3><b>Modifikationen der Kontrollprobe</b><br></h3>"
 	inputDialog += divFlexStart + `
             <label for="ZfPboost">ZfP* für Kontrollprobe verwenden</label><input type="checkbox" id="ZfPboost" name="ZfPboost" style="float:right">
-        `+ divFlexEnd + hr;
-	inputDialog += "<h3><b>" + "Vor- und Nachteile" + "</b><br>" + "</h3>";
-	inputDialog += divFlexStart + `
-            <label for="checkAffinity">Affinität zu Elementaren</label><input type="checkbox" id="checkAffinity" name="checkAffinity" style="float:right">
         `+ divFlexEnd;
 	inputDialog += divFlexStart + `
-            <label for="checkAntiGift">Begabung/Merkmalskenntnis (Gegenelement)</label><input type="checkbox" id="checkAntiGift" name="checkAntiGift" style="float:right">
-        `+ divFlexEnd;
-	inputDialog += divFlexStart + `
-            <label for="checkGift">Begabung/Merkmalskenntnis (gerufenes Element)</label><input type="checkbox" id="checkGift" name="checkGift" style="float:right">
+            <form action"#">
+                 <label for="Order">Gesinnung des Befehls</label>
+                 <select name="Order" id="Order" style="float:right">
+                    <option value="0">Bevorzugte Dienste</option>
+                    <option value="1" selected>Neutrale Dienste</option>
+                    <option value="2">unwillige Dienste</option>
+                 </select>
+            </form>
         `+ divFlexEnd + hr;
-	inputDialog += "<h3><b>" + "Spontane Modifikationen" + "</b><br>" + "</h3>";
+	inputDialog += "<h3><b>Spontane Modifikationen</b><br></h3>";
     inputDialog += divFlexStart + `
             <form action"#">
                  <label for="reduceAsP">Kosten einsparen (+3 ZfP pro 10 %)</label>
@@ -132,9 +187,6 @@ async function main() {
 	inputDialog += divFlexStart + `
             <label for="checkdoubledTime">Verdoppelte Zauberdauer (nur einmalig)</label><input type="checkbox" id="checkdoubledTime" name="checkdoubledTime" style="float:right">
         `+ divFlexEnd;
-	inputDialog += divFlexStart + `
-            <label for="checkMage">Repräsentation: Gildenmagier</label><input type="checkbox" id="checkMage" name="checkMage" style="float:right">
-        `+ divFlexEnd + hr;
 		
 //###############################################################################################################################################################################################################################################	
 
@@ -163,20 +215,17 @@ async function main() {
 		const elementInput = Number(html.find("#SelectElement")[0]?.value || 0);		//Auswahl des Elements
 		const controlDiffMod = Number(html.find("#controlDiff")[0]?.value || 0);		//Kontrollschwierigkeit
 		const reduceAsPInput = Number(html.find("#reduceAsP")[0]?.value || 0);			//AsP Kosten einsparen
+		const OrderInput = Number(html.find("#Order")[0]?.value || 0);					//Gesinnung des Befehls
+		const PurityInput = Number(html.find("#purity")[0]?.value || 0);				//Reinheit und MEnge des Elements
 		
 		//Checkboxes
-		const checkGiftInput = html.find("#checkGift")[0].checked;						//Begabung/Merkmalskenntnis (gerufenes Element)
-				GiftMod = (checkGiftInput === true)? 2 : 0;								//Zuweisen eines Wertes für aktivierte Checkbox
-		const checkAntiGiftInput = html.find("#checkAntiGift")[0].checked;				//Begabung/Merkmalskenntnis (Gegenelement)
-				AntiGiftMod = (checkAntiGiftInput === true)? 2 : 0;						//Zuweisen eines Wertes für aktivierte Checkbox
-		const checkAffinityInput = html.find("#checkAffinity")[0].checked;				//Affinität zu Elementaren
-				AffinityMod = (checkAffinityInput === true)? 3 : 0;						//Zuweisen eines Wertes für aktivierte Checkbox
 		const ZfPboostInput = html.find("#ZfPboost")[0].checked;						//ZfP* für Kontrollprobe verwenden
 		const checkhalfTimeInput = html.find("#checkhalfTime")[0].checked;				//Halbierte Zauberdauer (mindestens 1 Aktion)
 				halfTimeMod = (checkhalfTimeInput === true)? 5 : 0;						//Zuweisen eines Wertes für aktivierte Checkbox
 		const checkdoubledTimeInput = html.find("#checkdoubledTime")[0].checked;		//Verdoppelte Zauberdauer (nur einmalig)
 				doubledTimeMod = (checkdoubledTimeInput === true)? 3 : 0;				//Zuweisen eines Wertes für aktivierte Checkbox
-		const checkMageInput = html.find("#checkMage")[0].checked;						//Repräsentation:Gildenmagier
+		const checkGarmentInput = html.find("#checkGarment")[0].checked;				//richtige Gewandung
+				GarmentMod = (checkGarmentInput === true)? 2 : 0;						//Zuweisen eines Wertes für aktivierte Checkbox
 
 		
 		//Dropdown
@@ -184,26 +233,38 @@ async function main() {
             case 0:
                 ZSValue = ZSFireValue;
 				Elementargeist = " <b>des Feuers</b>";
+				GiftMod = Math.max(MKFireMod, BGFireMod);
+				AntiGiftMod = Math.max(MKWaterMod, BGWaterMod);
 				break;
             case 1:
 				ZSValue = ZSWaterValue;
 				Elementargeist = " <b>des Wassers</b>";
+				GiftMod = Math.max(MKWaterMod, BGWaterMod);
+				AntiGiftMod = Math.max(MKFireMod, BGFireMod);
 				break;
             case 2:
 				ZSValue = ZSHumusValue;
 				Elementargeist = " <b>des Humus</b>";
+				GiftMod = Math.max(MKHumusMod, BGHumusMod);
+				AntiGiftMod = Math.max(MKIceMod, BGIceMod);
 				break;
 			case 3:
 				ZSValue = ZSIceValue;
 				Elementargeist = " <b>des Eises</b>";
+				GiftMod = Math.max(MKIceMod, BGIceMod);
+				AntiGiftMod = Math.max(MKHumusMod, BGHumusMod);
 				break;
             case 4:
 				ZSValue = ZSOreValue;
 				Elementargeist = " <b>des Erzes</b>";
+				GiftMod = Math.max(MKOreMod, BGOreMod);
+				AntiGiftMod = Math.max(MKAirMod, BGAirMod);
 				break;
             case 5:
 				ZSValue = ZSAirValue;
 				Elementargeist = " <b>der Luft</b>";
+				GiftMod = Math.max(MKAirMod, BGAirMod);
+				AntiGiftMod = Math.max(MKOreMod, BGOreMod);
 				break;
         }
 
@@ -240,6 +301,42 @@ async function main() {
 				break;
         }
 		
+		switch(OrderInput){
+			case 0:
+				Orderfactor = 3;
+				break;
+			case 1:
+				Orderfactor = 0;
+				break;
+			case 2:
+				Orderfactor = -7;
+				break;
+		}
+				
+		switch(PurityInput){
+			case 0:
+				Purityfactor = -3;
+				break;
+			case 1:
+				Purityfactor = -2;
+				break;
+			case 2:
+				Purityfactor = -1;
+				break;
+			case 3:
+				Purityfactor = 0;
+				break;
+			case 4:
+				Purityfactor = 1;
+				break;
+			case 5:
+				Purityfactor = 2;
+				break;
+			case 6:
+				Purityfactor = 3;
+				break;
+		}
+		
 		//Spell
 		const ZfW = ElementarerDienerValue;
 		
@@ -247,9 +344,10 @@ async function main() {
             win = " beschwört einen <b>Elementargeist</b>";
 			AsPcosts = Math.round((12 - KraftkontrolleValue) * reductionFactor); 
 
-			if(checkMage === true){
-				mod = summonDiffMod - trueNameMod - ElementaristValue - GiftMod + AntiGiftMod - (doubledTimeMod + 1) + Math.round((halfTimeMod + reduceAsPMod)/2)
-			}else{mod = summonDiffMod - trueNameMod - ElementaristValue - GiftMod + AntiGiftMod - doubledTimeMod + halfTimeMod + reduceAsPMod
+			if(checkMageMod = true){
+				mod = summonDiffMod - trueNameMod - ElementaristValue - GiftMod + AntiGiftMod - (doubledTimeMod + 1) + Math.round((halfTimeMod + reduceAsPMod)/2) + Purityfactor - GarmentMod
+			}else{
+				mod = summonDiffMod - trueNameMod - ElementaristValue - GiftMod + AntiGiftMod - doubledTimeMod + halfTimeMod + reduceAsPMod + Purityfactor - GarmentMod
 			}
 			modOutput = (mod >=0)? "+" + mod: mod;
 
@@ -302,7 +400,7 @@ async function main() {
 			
 			//Berechnung des Kontrollwertes
 			Control = Math.round((courage + intuition + charisma + charisma + eZfW)/5)	
-				eControl = Control - controlDiffMod + AffinityMod + GiftMod - (AntiGiftMod * 2) + ZfPboostMod;
+				eControl = Control - controlDiffMod + AffinityMod + GiftMod - (AntiGiftMod * 2) + ZfPboostMod + Orderfactor;
 					failControl = ", der von ihm aber nicht kontrolliert werden kann";
 					winControl = " und kann diesen kontrollieren";
 
@@ -337,7 +435,6 @@ async function main() {
 				token.actor.update({'system.base.resources.astralEnergy.value':aspUpdate});
 			}
 
-
 			//Chat Output
             flavor = "<b> Beschwörung des Elementars </b><br>";
 			flavor += "MU: " + w1 + " / " + "<b>" + courage + "</b>" + " (" + resOne + ")";
@@ -354,6 +451,55 @@ async function main() {
 				flavor += "<br>" + hr;
 				flavor += "<b>" + tokenName + "</b>" + failOut + hr;
 			}
+			flavor += "<u>Vor- und Nachteile</u><br>";
+			if(AffinityMod === 3){
+				flavor += "Affinität zu  Elementaren<br>";
+			}if(BGFireMod === 2){
+				flavor += "Begabung für Elementar (Feuer)<br>";
+			}if(BGWaterMod === 2){
+				flavor += "Merkmalskenntnis: Elementar (Wasser)<br>";
+			}if(BGHumusMod === 2){
+				flavor += "Begabung für Elementar (Humus)<br>";
+			}if(BGIceMod === 2){
+				flavor += "Begabung für Elementar (Eis)<br>";
+			}if(BGOreMod === 2){
+				flavor += "Begabung für Elementar (Erz)<br>";
+			}if(BGAirMod === 2){
+				flavor += "Begabung für Elementar (Luft)<br>";
+			}if(checkMageMod === true){
+				flavor += "Repräsentation: Magier<br>";
+			}
+			flavor += "<u>Sonderfertigkeiten</u><br>";
+			if(MKFireMod === 2){
+				flavor += "Merkmalskenntnis: Elementar (Feuer)<br>";
+			}if(MKWaterMod === 2){
+				flavor += "Merkmalskenntnis: Elementar (Wasser)<br>";
+			}if(MKHumusMod === 2){
+				flavor += "Merkmalskenntnis: Elementar (Humus)<br>";
+			}if(MKIceMod === 2){
+				flavor += "Merkmalskenntnis: Elementar (Eis)<br>";
+			}if(MKOreMod === 2){
+				flavor += "Merkmalskenntnis: Elementar (Erz)<br>";
+			}if(MKAirMod === 2){
+				flavor += "Merkmalskenntnis: Elementar (Luft)<br>";
+			}if(ZSFireValue === 2){
+				flavor += "Zauberspezialisierung: Elementarer Diener (Elementar (Feuer))<br>";
+			}if(ZSWaterValue === 2){
+				flavor += "Zauberspezialisierung: Elementarer Diener (Elementar (Wasser))<br>";
+			}if(ZSHumusValue === 2){
+				flavor += "Zauberspezialisierung: Elementarer Diener (Elementar (Humus))<br>";
+			}if(ZSIceValue === 2){
+				flavor += "Zauberspezialisierung: Elementarer Diener (Elementar (Eis))<br>";
+			}if(ZSOreValue === 2){
+				flavor += "Zauberspezialisierung: Elementarer Diener (Elementar (Erz))<br>";
+			}if(ZSAirValue === 2){
+				flavor += "Zauberspezialisierung: Elementarer Diener (Elementar (Luft))<br>";
+			}if(KraftkontrolleValue === 1){
+				flavor += "Kraftkontrolle<br>";
+			}if(ElementaristValue === 3){
+				flavor += "Elementarist<br>";
+			}
+				
             
             roll.toMessage ({
                 flavor: flavor,
